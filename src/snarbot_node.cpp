887e5f94eadca6d69baa658b9b416d62c9fe75ad
CORPSE <corpse@CORPSE.PC>
2021-04-02 15:26:42 +0300

    first commit

---
+++ src/snarbot_node.cpp
@@ -0,0 +1,21 @@
+/*#include<ros/ros.h>
+#include<std_msgs/Header.h>
+#include<geometry_msgs/Twist.h>
+#include<sstream>
+
+void message_callback(){
+    
+}
+
+int main(int argv, char **argc){
+
+    ros::init(argv, argc, "first_node");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe<geometry_msgs::Twist>("twister", 1000, message_callback);
+
+
+}*/
+
+int main(){
+    return 0;
+}
